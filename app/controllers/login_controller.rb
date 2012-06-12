0366217d02f085365a2742d9d0ef9411ae428b51
XEdP3X <xedp3x@online.de>
2012-06-12 20:21:21 +0200

    Password ändern und init Link

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -16,4 +16,36 @@ class LoginController < ApplicationController
     reset_session
     redirect_to :action => "index"
   end
+  
+  def update
+    if params[:register] then
+      @user = User.authenticate(params[:email],params[:register])
+      if not @user then
+        flash[:error] = "Der Link ist nicht mehr gültig!"
+        redirect_to :controller => "login", :action => "index"
+      end
+      @register = params[:register]
+    else if session[:user_id].blank? then
+        redirect_to :controller => "login", :action => "index"
+        return
+      end
+      @user = User.find(session[:user_id])
+    end
+    
+    if params[:old]
+      if params[:new] != params[:new2]
+        @error = "Die Passwordwiederholung ist falsch"
+      elsif params[:new].length < 8 then
+        @error = "Ihr Password ist zu kurz" 
+      elsif @user.update_pass(params[:old],params[:new]) then
+         flash[:notice] = "Password wurde geändert"
+         redirect_to :controller => "home", :action => "index"
+         return
+       else
+         @error = "Ihr altes Password ist falsch"
+      end
+    end    
+    @user = false if params[:register]
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   
     require 'digest/sha1'
     def pass= (pass)
+      return false if self.pass == pass
       write_attribute("pass", Digest::SHA1.hexdigest(pass))
       Log.create(:user_id => self.id, :instants_id => 0, :action => "user", :parameters => "set passwort")
     end
@@ -11,6 +12,7 @@ class User < ActiveRecord::Base
     def update_pass(old, pass)
       return false if self.pass != Digest::SHA1.hexdigest(old)
       self.pass= pass;
+      self.save
       return true
     end
     
@@ -18,7 +20,7 @@ class User < ActiveRecord::Base
       return nil if email.nil? or pass.nil?
       back = find_by_email_and_pass(email,Digest::SHA1.hexdigest(pass))
       if back.nil? then
-        Log.create(:user_id => 0, :instants_id => 0, :action => "login_false", :parameters => "#{email}")
+        Log.create(:user_id => 0, :instants_id => 0, :action => "login_false", :parameters => email)
       else
         Log.create(:user_id => back.id, :instants_id => 0, :action => "user", :parameters => "login")
       end
